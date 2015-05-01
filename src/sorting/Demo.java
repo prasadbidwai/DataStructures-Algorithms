418696034d85c5799a158b3e8079e99164198d08
prasadbidwai <prasadbidwai@gmail.com>
2015-05-01 12:46:40 -0700

    Basic sorting algorithms

---
+++ src/sorting/Demo.java
@@ -0,0 +1,17 @@
+package sorting;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		Sort s= new Sort();
+		//s.selectSort();
+		//.s.bubbleSort();
+		//s.display();
+		System.out.println("now swlwction sort: ");
+		s.selectSort();
+		s.display();
+		
+	}
+	
+}
---
+++ src/sorting/Sort.java
@@ -0,0 +1,66 @@
+package sorting;
+
+public class Sort {
+	
+	int[] a = {7,8,9,4,3,2,5,1};
+	int in,out;
+
+	int len=a.length;
+	
+	public void bubbleSort() {
+		for(out=len-1;out>0;out--)
+		{
+			for(in=0;in<out;in++)
+			{
+				if(a[in]>a[in+1])
+					swap(in,(in+1));
+				
+			}
+			
+		}
+	}
+
+	public void selectSort()
+	{
+		for (int i = 0; i < a.length; i++) {
+	        // Assume first element is min
+	      int  min = i;
+	        for (int j = i + 1; j < a.length; j++) {
+	            if (a[j] < a[min]) {
+	                min = j;
+
+	            }
+	        }
+	        if (min != i) {
+	            swap(i,min);
+	        }
+	            
+	    }
+	}
+
+	public void swap(int one, int two)
+	{
+	int temp = a[one];
+	a[one] = a[two];
+	a[two] = temp;
+	}
+
+	public void display() {
+		for(int i=0;i<len;i++)
+		{
+			
+			System.out.println(" "+a[i]);
+			
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
+
+
+
+
+		
