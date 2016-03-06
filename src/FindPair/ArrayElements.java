65cfb778c029367ae0b2345a7bf43d76a9cb985d
Prasad Bidwai <prasadbidwai@gmail.com>
2016-03-06 11:45:04 -0800

    some minor changes

--- src/FindPair/ArrayElements.java
+++ src/FindPair/ArrayElements.java
@@ -13,22 +13,18 @@ class ArrayElements{
  
         // Traverse through all possible pairs of arr[]
        for (int i = 0; i < n-1; i++){
-
        	for (int j = i+1; j < n-1; j++){
 
        		int sum = arr[i] + arr[j];
-
-       		if(!map.containsKey(sum)){
+       		if(!map.containsKey(sum))
+       		{
        			map.put(sum, new Pair(arr[i],arr[j]));
-      // 			return false;
        		}
 
        		else{
        			Pair found = map.get(sum);
-       		
-       			System.out.println("the pair with sum similar to " + arr[i] + ", " + arr[j] + " is: "+ found.first + ", " + found.second);
-
-       			return true;
+   		  	System.out.println("the pair with sum similar to " + arr[i] + ", " + arr[j] + " is: "+ found.first + ", " + found.second);
+			return true;
        		}
        	}
        }
