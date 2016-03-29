a9fd8123d5e2f580c3be48e8f0a4948e65f6059c
prasadbidwai <prasadbidwai@gmail.com>
2016-03-29 11:09:26 -0700

    adding one more SQL question

--- src/SQL Questions/SqlQuestion.sql
+++ src/SQL Questions/SqlQuestion.sql
@@ -11,3 +11,40 @@ where
     emp.DepartmentId=dep.Id 
 and  
 (Select count(distinct Salary) From Employee where DepartmentId=dep.Id and Salary>emp.Salary)<3
+
+
+-- 2. Department wise highest salary 
+
+/*
+Employee table: 
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+
+Department Table: 
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+
+*/
+select distinct(d.Name) as Department, f.name as Employee, r.maxsalary as Salary 
+from
+(select 
+    e.DepartmentId, max(e.salary)as maxsalary
+from 
+    Employee e
+group by 1)r, Employee f, Department d  
+where 
+    r.maxsalary = f.salary
+and 
+    r.DepartmentId = f.DepartmentId 
+and 
+    f.DepartmentId = d.id 
