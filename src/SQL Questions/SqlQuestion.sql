7ea57443dd25c60ddd043eb7186771f79057396c
prasadbidwai <prasadbidwai@gmail.com>
2016-03-29 10:59:44 -0700

    Adding SQL questions

---
+++ src/SQL Questions/SqlQuestion.sql
@@ -0,0 +1,13 @@
+
+-- 1. find top 3 salaries per department
+
+Select dep.Name as Department
+,emp.Name as Employee
+,emp.Salary 
+from 
+    Department dep, 
+    Employee emp 
+where  
+    emp.DepartmentId=dep.Id 
+and  
+(Select count(distinct Salary) From Employee where DepartmentId=dep.Id and Salary>emp.Salary)<3
