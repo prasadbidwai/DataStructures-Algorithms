292b1b800c4195f6c99f8b9bfd7324c5b1caa855
prasadbidwai <prasadbidwai@gmail.com>
2016-03-29 11:14:32 -0700

    adding detailed description of SQL questions

--- src/SQL Questions/SqlQuestion.sql
+++ src/SQL Questions/SqlQuestion.sql
@@ -1,6 +1,39 @@
 
 -- 1. find top 3 salaries per department
 
+/*
+Employee table:
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
++----+-------+--------+--------------+
+
+Department table: 
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+
+Result should be like : 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Randy    | 85000  |
+| IT         | Joe      | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+*/
+
 Select dep.Name as Department
 ,emp.Name as Employee
 ,emp.Salary 
