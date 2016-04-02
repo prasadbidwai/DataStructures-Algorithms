b2c8b668c08caa9a15070284ba584db5d193cf9d
prasadbidwai <prasadbidwai@gmail.com>
2016-04-01 22:58:21 -0700

    adding new question

--- src/SQL Questions/SqlQuestion.sql
+++ src/SQL Questions/SqlQuestion.sql
@@ -106,3 +106,22 @@ BEGIN
   );
 END
 
+/*
+4. Write a SQL query to find all numbers that appear at least three times consecutively.
+
++----+-----+
+| Id | Num |
++----+-----+
+| 1  |  1  |
+| 2  |  1  |
+| 3  |  1  |
+| 4  |  2  |
+| 5  |  1  |
+| 6  |  2  |
+| 7  |  2  |
++----+-----+
+*/
+
+select distinct l1.Num from Logs l1, Logs l2, Logs l3
+where l1.Id = l2.id-1 and l2.Id = l3.Id-1
+and l1.Num = l2.Num and l2.Num = l3.Num
