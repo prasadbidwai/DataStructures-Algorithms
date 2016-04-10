9eabd5beac3846a461ea0390c4a14e21b74f5e4a
prasadbidwai <prasadbidwai@gmail.com>
2016-04-10 16:14:30 -0700

    adding more sql questions

--- src/SQL Questions/SqlQuestion.sql
+++ src/SQL Questions/SqlQuestion.sql
@@ -126,3 +126,21 @@ where l1.Id = l2.id-1 and l2.Id = l3.Id-1
 and l1.Num = l2.Num and l2.Num = l3.Num;
 
 
+/*
+5. The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+*/
