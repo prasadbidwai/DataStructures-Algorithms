07e1309be9ab9313eeb045a628ae2b18be2bbc3e
prasadbidwai <prasadbidwai@gmail.com>
2016-04-12 12:05:39 -0700

    new question

--- src/SQL Questions/SqlQuestion.sql
+++ src/SQL Questions/SqlQuestion.sql
@@ -167,3 +167,33 @@ Person p1, Person p2
 where 
 p1.Id > p2.Id and
 p1.Email = p2.Email;
+
+
+/*
+7. Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+
+Answer should be like:
+
++-------+------+
+| Score | Rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+
+
+*/
