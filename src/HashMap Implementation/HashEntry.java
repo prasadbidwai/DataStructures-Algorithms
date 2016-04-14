dbdfc461332e2a4768326e18c31e818c46d544c1
prasadbidwai <prasadbidwai@gmail.com>
2016-04-14 01:03:19 -0700

    adding hashmap implementation

---
+++ src/HashMap Implementation/HashEntry.java
@@ -0,0 +1,21 @@
+class HashEntry
+{
+	private int key;
+	private int value;
+
+	public HashEntry(int a, int b)
+	{
+		key = a;
+		valueOf = b;
+	}
+
+	public int getKey()
+	{
+		return key;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+}
