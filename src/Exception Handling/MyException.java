ca5388d73d7c1bea5a6185fc2dc1506f5b2acfb3
prasadbidwai <prasadbidwai@gmail.com>
2016-05-09 22:22:50 -0700

    code refactoring

--- src/Exception Handling/MyException.java
+++ src/Exception Handling/MyException.java
@@ -1,14 +1,23 @@
-This program creates a custom exception type.
-class MyException extends Exception {
-private int detail;
-MyException(int a) {
-detail = a;
+//This program creates a custom exception type.
+
+class MyException extends Exception 
+{
+	private int detail;
+	
+	MyException(int a) 
+	{
+		detail = a;
+	}
+
+	public String toString() 
+	{
+		return "MyException[" + detail + "]";
+	}
+
 }
-public String toString() {
-return "MyException[" + detail + "]";
-}
-}
-class ExceptionDemo {
+
+class ExceptionDemo 
+{
 static void compute(int a) throws MyException {
 System.out.println("Called compute(" + a + ")");
 if(a > 10)
