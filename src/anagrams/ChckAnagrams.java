8fcc6b70d23e387d2bd74224ef706205979d4634
prasadbidwai <prasadbidwai@gmail.com>
2016-05-12 13:58:18 -0700

    adding multiple ways to solve the problem

--- src/anagrams/ChckAnagrams.java
+++ src/anagrams/ChckAnagrams.java
@@ -9,35 +9,74 @@ public String sort1(String s1){
 	return String.valueOf(s1c);
 }
 	
-public boolean chckStrings(String s1, String s2){
-
-	if(s1.length() != s2.length()){
+public boolean chckStrings(String s1, String s2)
+{
+	String s1c = sort1(s1.toLowerCase().replaceAll("\\s", ""));
+	String s2c = sort1(s2.toLowerCase().replaceAll("\\s", ""));
+	
+	if(s1c.length() != s2c.length())
+	{	
+		System.out.println(s1c.length()+ " "+s2c.length() + "lenghts are not same");
 		return false;
 	}
-	
-	String s1c = sort1(s1.toLowerCase());
-
-	s1c = s1c.replaceAll("\\s", "");	//Removes any white spaces present in string 
-
-	String s2c = sort1(s2.toLowerCase());
-
-	s2c = s2c.replaceAll("\\s", "");	//Removes any white spaces present in string
-
 	System.out.println(s1c+"  "+s2c);
-
 	return s1c.equals(s2c);
-	
-	}	
-	
-public static void main(String[] args){
-	
-	ChckAnagrams cka = new ChckAnagrams();
-
-	System.out.println(cka.chckStrings("Mother In Law", "Hitler Woman"));
-	System.out.println(cka.chckStrings("a bcdef", "fdea bc"));
-	System.out.println(cka.chckStrings("SiLeNt CAT", "LisTen AcT"));
-	
 }
 
+//Method 2 using String builder
+
+static void isAnagram(String s1, String s2) 	
+{
+    String copyOfs1 = s1.replaceAll("\\s", "").toLowerCase();
+    String copyOfs2 = s2.replaceAll("\\s", "").toLowerCase();
+ 
+    boolean status = true;
+ 
+        if(copyOfs1.length() != copyOfs2.length())
+        {
+            status = false;
+        }
+        else
+        {
+            char[] s1Array = copyOfs1.toCharArray();
+            StringBuilder sb = new StringBuilder(copyOfs2);
+ 
+            for (char c : s1Array)
+            {
+                int index = sb.indexOf(String.valueOf(c));
+                System.out.println(c+":  "+index);
+ 
+                if (index != -1)
+                {
+                    sb = sb.deleteCharAt(index);
+                }
+                else
+                {
+                    status = false;
+                    break;
+                }
+            }
+        } 
+        if(status)
+        {
+            System.out.println(s1+" and "+s2+" are anagrams");
+        }
+        else
+        {
+            System.out.println(s1+" and "+s2+" are not anagrams");
+        }
+    }	
+	
+	public static void main(String[] args)
+	{
+	
+		ChckAnagrams cka = new ChckAnagrams();
+
+		System.out.println(cka.chckStrings("Mother In Law", "Hitler Woman"));
+		System.out.println(cka.chckStrings("a bcdef", "fdea bc"));
+		System.out.println(cka.chckStrings("SiLeNt CAT", "LisTen AcT"));
+
+		isAnagram("Mother In Law ", "Hitler Woman");
+	}
 }
 
