dc9efddab0985a12eb2a6ef8d9640842cea2adf1
prasadbidwai <prasadbidwai@gmail.com>
2016-05-12 12:54:37 -0700

    adding better method for solution

--- src/anagrams/ChckAnagrams.java
+++ src/anagrams/ChckAnagrams.java
@@ -1,12 +1,12 @@
-package anagrams;
+import java.util.*;
 
 public class ChckAnagrams {
 
 public String sort1(String s1){
 	
 	char[] s1c = s1.toCharArray();
-	java.util.Arrays.sort(s1c);
-	return new String(s1c);
+	Arrays.sort(s1c);
+	return String.valueOf(s1c);
 }
 	
 public boolean chckStrings(String s1, String s2){
@@ -15,9 +15,16 @@ public boolean chckStrings(String s1, String s2){
 		return false;
 	}
 	
-	String s1c = sort1(s1);
-	String s2c = sort1(s2);
-	
+	String s1c = sort1(s1.toLowerCase());
+
+	s1c = s1c.replaceAll("\\s", "");	//Removes any white spaces present in string 
+
+	String s2c = sort1(s2.toLowerCase());
+
+	s2c = s2c.replaceAll("\\s", "");	//Removes any white spaces present in string
+
+	System.out.println(s1c+"  "+s2c);
+
 	return s1c.equals(s2c);
 	
 	}	
@@ -25,8 +32,12 @@ public boolean chckStrings(String s1, String s2){
 public static void main(String[] args){
 	
 	ChckAnagrams cka = new ChckAnagrams();
-	System.out.println(cka.chckStrings("abcdef", "fdeabc"));
+
+	System.out.println(cka.chckStrings("Mother In Law", "Hitler Woman"));
+	System.out.println(cka.chckStrings("a bcdef", "fdea bc"));
+	System.out.println(cka.chckStrings("SiLeNt CAT", "LisTen AcT"));
 	
 }
 
 }
+
