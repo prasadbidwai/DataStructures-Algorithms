485dba97bf290d615570cba84acfe6135ebdbeec
prasadbidwai <prasadbidwai@gmail.com>
2016-05-30 17:36:22 -0700

    adding new design pattern

---
+++ src/Decorator/Beverage - Copy.java
@@ -0,0 +1,14 @@
+public abstract class Beverage1
+{
+	String description = "NO description available for";
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public abstract int cost();
+
+}
+
+
---
+++ src/Decorator/Beverage.java
@@ -0,0 +1,14 @@
+public abstract class Beverage
+{
+	String description = "NO description available for";
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public abstract int cost();
+
+}
+
+
