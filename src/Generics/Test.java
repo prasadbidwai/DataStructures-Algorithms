3606fd9c53b5c2e3116bbd117faaf99b51df0c41
prasadbidwai <prasadbidwai@gmail.com>
2016-06-07 19:27:15 -0700

    adding generics function example code

--- src/Generics/Test.java
+++ src/Generics/Test.java
@@ -80,3 +80,39 @@ Output:
 15
 GeeksForGeeks
 */
+
+// A Simple Java program to show working of user defined
+// Generic functions
+  
+class Test
+{
+    // A Generic method example
+    static <T> void genericDisplay (T element)
+    {
+        System.out.println(element.getClass().getName() +
+                           " = " + element);
+    }
+  
+    // Driver method
+    public static void main(String[] args)
+    {
+         // Calling generic method with Integer argument
+        genericDisplay(11);
+  
+        // Calling generic method with String argument
+        genericDisplay("GeeksForGeeks");
+  
+        // Calling generic method with double argument
+        genericDisplay(1.0);
+    }
+}
+
+
+/*
+Output :
+
+java.lang.Integer = 11
+java.lang.String = GeeksForGeeks
+java.lang.Double = 1.0
+
+*/
