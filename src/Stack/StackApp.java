0f7c64e806c14c4cc6e5082514638be3067533c6
prasadbidwai <prasadbidwai@gmail.com>
2016-06-30 11:23:08 -0700

    code refactoring

--- src/Stack/StackApp.java
+++ src/Stack/StackApp.java
@@ -1,19 +1,19 @@
 class StackApp{
 	
-	public StackApp(){
 
-		Stackx op = new Stackx(4);
-	}
+	public static void main(String[] args) {
+	
+	Stackx op = new Stackx();
 
 		op.push(1);
 		op.push(2);
 		op.push(3);
 		op.push(4);
 
-		while(!op.isEmpty){
+		while(!op.isEmpty()){
 
 			int thisOne = op.pop();
 			System.out.println(thisOne+ " ");
 		}
-	
+		}		
 }
