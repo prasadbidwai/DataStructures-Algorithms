12fb9f8a3be3b06bbee50b8e7def007b73f77059
prasadbidwai <prasadbidwai@gmail.com>
2016-07-04 19:00:17 -0700

    code refactoring

--- src/anagrams/ChckAnagrams.java
+++ src/anagrams/ChckAnagrams.java
@@ -3,33 +3,33 @@ import java.util.*;
 public class ChckAnagrams {
 
 public String sort1(String s1){
-	
-	char[] s1c = s1.toCharArray();
-	Arrays.sort(s1c);
-	return String.valueOf(s1c);
+    
+    char[] s1c = s1.toCharArray();
+    Arrays.sort(s1c);
+    return String.valueOf(s1c);
 }
-	
+    
 public boolean chckStrings(String s1, String s2)
 {
-	String s1c = sort1(s1.toLowerCase().replaceAll("\\s", ""));
-	String s2c = sort1(s2.toLowerCase().replaceAll("\\s", ""));
-	
-	if(s1c.length() != s2c.length())
-	{	
-		System.out.println(s1c.length()+ " "+s2c.length() + "lenghts are not same");
-		return false;
-	}
-	System.out.println(s1c+"  "+s2c);
-	return s1c.equals(s2c);
+    String s1c = sort1(s1.toLowerCase().replaceAll("\\s", ""));
+    String s2c = sort1(s2.toLowerCase().replaceAll("\\s", ""));
+    
+    if(s1c.length() != s2c.length())
+    {   
+        System.out.println(s1c.length()+ " "+s2c.length() + "lenghts are not same");
+        return false;
+    }
+    System.out.println(s1c+"  "+s2c);
+    return s1c.equals(s2c);
 }
 
 //Method 2 using String builder
 
-static void isAnagram(String s1, String s2) 	
+static void isAnagram(String s1, String s2)     
 {
     String copyOfs1 = s1.replaceAll("\\s", "").toLowerCase();
     String copyOfs2 = s2.replaceAll("\\s", "").toLowerCase();
- 
+
     boolean status = true;
  
         if(copyOfs1.length() != copyOfs2.length())
@@ -44,7 +44,7 @@ static void isAnagram(String s1, String s2)
             for (char c : s1Array)
             {
                 int index = sb.indexOf(String.valueOf(c));
-                System.out.println(c+":  "+index);
+ //               System.out.println(c+":  "+index);
  
                 if (index != -1)
                 {
@@ -65,18 +65,25 @@ static void isAnagram(String s1, String s2)
         {
             System.out.println(s1+" and "+s2+" are not anagrams");
         }
-    }	
-	
-	public static void main(String[] args)
-	{
-	
-		ChckAnagrams cka = new ChckAnagrams();
+        //edit
+    }
 
-		System.out.println(cka.chckStrings("Mother In Law", "Hitler Woman"));
-		System.out.println(cka.chckStrings("a bcdef", "fdea bc"));
-		System.out.println(cka.chckStrings("SiLeNt CAT", "LisTen AcT"));
 
-		isAnagram("Mother In Law ", "Hitler Woman");
-	}
+    
+    public static void main(String[] args)
+    {
+    
+        ChckAnagrams cka = new ChckAnagrams();
+
+        System.out.println(cka.chckStrings("Mother In Law", "Hitler Woman"));
+        System.out.println(cka.chckStrings("a bcdef", "fdea bc"));
+        System.out.println(cka.chckStrings("SiLeNt CAT", "LisTen AcT"));
+
+        System.out.println("\nIs Anagram Method ::");
+
+        isAnagram("Mother In Law ", "Hitler Woman");
+        isAnagram("aaabbbccd", "bbbccd aaa");
+
+    }
 }
 
