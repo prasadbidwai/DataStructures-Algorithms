bbaacffd8ae440b6330ed2a882eaff2790a63e65
prasadbidwai <prasadbidwai@gmail.com>
2016-07-30 11:35:28 -0700

    new function to demonstrate generics

--- src/Generics/Test.java
+++ src/Generics/Test.java
@@ -1,5 +1,36 @@
-// A Simple Java program to show working of user defined
-// Generic classes
+// A Simple Java program to show working of user defined Generic classes
+
+// Example to show user defined generics function: 
+
+public class Test
+{
+   // generic method printArray                         
+   public static < E > void printArray( E[] inputArray )
+   {
+      // Display array elements              
+         for ( E element : inputArray ){        
+            System.out.printf( "%s ", element );
+         }
+         System.out.println();
+    }
+
+    public static void main( String args[] )
+    {
+        // Create arrays of Integer, Double and Character
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+        System.out.println( "Array integerArray contains:" );
+        printArray( intArray  ); // pass an Integer array
+
+        System.out.println( "\nArray doubleArray contains:" );
+        printArray( doubleArray ); // pass a Double array
+
+        System.out.println( "\nArray characterArray contains:" );
+        printArray( charArray ); // pass a Character array
+    } 
+}
   
 // We use < > to specify Parameter type
 class Test<T>
@@ -81,8 +112,7 @@ Output:
 Test String
 */
 
-// A Simple Java program to show working of user defined
-// Generic functions
+// A Simple Java program to show working of user defined Generic functions
   
 class Test
 {
