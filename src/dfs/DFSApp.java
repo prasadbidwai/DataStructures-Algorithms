c45d56bfdeb79d556becba99655d1fb7d4dcecba
Prasad Bidwai <prasadbidwai@gmail.com>
2017-01-22 18:57:08 -0800

    update in appDemo file

--- src/dfs/DFSApp.java
+++ src/dfs/DFSApp.java
@@ -10,21 +10,33 @@ class DFSApp{
 		tg.addVertex('C'); // 2
 		tg.addVertex('D'); // 3
 		tg.addVertex('E'); // 4
+		tg.addVertex('F'); // 5
+		tg.addVertex('G'); // 6		
 		
 		tg.addEdge(0, 1); // AB
 		tg.addEdge(1, 2); // BC
 		tg.addEdge(0, 3); // AD
 		tg.addEdge(3, 4); // DE
+		tg.addEdge(1, 6); // BG
+		tg.addEdge(3, 5); // DF
 		
 //		tg.printGraph();
 //		tg.displayVertices();
 
 		System.out.println("Calling the DFS:: ");
-		tg.dfs(); // depth-first search
+		tg.dfs1(); // depth-first search
 		
 		System.out.println("\nCalling the BFS Now :: ");
-		tg.bfs();
+		tg.bfs1();
 
 		System.out.println();
 	}
 }
+
+
+//	The Tree:
+//			      A
+//			   /    \
+//			  B      D	
+//			 / \     / \
+//			C   G   E   F
