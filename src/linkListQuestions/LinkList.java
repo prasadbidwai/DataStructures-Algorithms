2ce0a88aa242040ddc74648ac31244607f1b70df
prasadbidwai <prasadbidwai@gmail.com>
2017-03-25 19:40:17 -0700

    new linklist operations

--- src/linkListQuestions/LinkList.java
+++ src/linkListQuestions/LinkList.java
@@ -34,6 +34,43 @@ public class LinkList {
 		}
 		System.out.print(" ");
 	}
+
+	public Link findLink(int key){
+
+		Link current = first;
+
+		while(current.idata != key){
+			if(current.next == null)
+				return null;
+
+			else 
+				current = current.next;
+		}
+		return current;
+	}
+
+	public Link deleteLink(int key){
+
+		Link current = first;
+		Link previous = first;
+		
+		while(current.idata != key){
+			
+			if (current.next == null)
+			return null;
+
+			else{
+
+				previous = current;
+				current = current.next;
+			}
+		}
+		if(current == first)
+			first = first.next;
+		else	
+			previous.next = current.next;
+		return current;
+	}	
 	
 	public void deleteDups(Link l1){
 		HashSet<Integer> hs = new HashSet<Integer>();
@@ -64,4 +101,31 @@ public class LinkList {
 	  }
 	  first = reversedPart;
 	}
+
+	public Link reverseBetween(Link head, int m, int n) {
+	    if(head == null) return null;
+	    Link dummy = new Link(0); // create a dummy node to mark the head of this list
+	    dummy.next = head;
+	    Link pre = dummy; // make a pointer pre as a marker for the node before reversing
+	    for(int i = 0; i<m-1; i++) pre = pre.next;
+	    
+	    Link start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
+	    Link then = start.next; // a pointer to a node that will be reversed
+	    
+	    // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+	    // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+	    for(int i=0; i<n-m; i++)
+	    {
+	        start.next = then.next;
+	        then.next = pre.next;
+	        pre.next = then;
+	        then = start.next;
+	    }
+	    
+	    // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+	    // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+	    return dummy.next;
+	}
+
+
 }
